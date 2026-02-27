2528119d722925a8529abf558fcb8368034e8216
Wemerson Oliveira <wemerson.contacto@gmail.com>
2026-02-27 16:36:50 -0300

    fix: clean project build

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import "dotenv/config"
 import express from "express"
-import communityRoutes from "./routes/community.routes"
 
 const app = express()
-
-app.use(express.json())
 
 app.get("/ping", (req, res) => {
   res.send("pong")
 })
-
-app.use(communityRoutes)
 
 export default app
