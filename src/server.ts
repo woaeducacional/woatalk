6d7f9b41dd122251b9788ff16c36d59c674e4b42
Wemerson Oliveira <wemerson.contacto@gmail.com>
2026-02-28 14:20:35 -0300

    fix: add root route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
-import "dotenv/config"
-import express from "express"
+import "dotenv/config";
+import express from "express";
 
-const app = express()
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("WOA Talk is live ✅");
+});
 
 app.get("/ping", (req, res) => {
-  res.send("pong")
-})
+  res.status(200).send("pong");
+});
 
-export default app
+export default app;
